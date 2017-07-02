4fd209fbe3f649eca686a8ec4caa7289c31d5917
Björn Rasmusson <B.Rasmusson@computer.org>
2017-07-02 11:24:02 +0200

    [Core] Use the test case duration in the JUnit Formatter.
    Let the JUnit Formatter use the duration of the test case result
    instead of summing up the durations of the test step results.
    
    Also refactor the TimeService to have the methods startTime(),
    currentTime(), and finishTime(). These methods actually do the same
    thing but having them all enables/simplifies the mocking of time in
    tests.

--- core/src/main/java/cucumber/runner/TimeService.java
+++ core/src/main/java/cucumber/runner/TimeService.java
@@ -1,28 +1,70 @@
 package cucumber.runner;
 
 public interface TimeService {
-    long time();
+    // The reason to have three method, which actually is doing the same thing,
+    // is to enable stubbing of the time service in tests. With the distinction
+    // between startTime() and finishTime() it is easy for the stub to make sure
+    // the the duration between the start time and finish time of the test case,
+    // is the sum of the durations of the test steps.
+    long startTime();
+    long currentTime();
+    long finishTime();
 
     TimeService SYSTEM = new TimeService() {
         @Override
-        public long time() {
+        public long startTime() {
+            return currentTime();
+        }
+
+        @Override
+        public long currentTime() {
             return System.nanoTime();
+        }
+
+        @Override
+        public long finishTime() {
+            return currentTime();
         }
     };
 
     class Stub implements TimeService {
         private final long duration;
         private final ThreadLocal<Long> currentTime = new ThreadLocal<Long>();
+        private boolean startTimeWasLastCall = false;
 
         public Stub(long duration) {
             this.duration = duration;
         }
 
         @Override
-        public long time() {
+        public long startTime() {
+            startTimeWasLastCall = true;
+            return getCurrentTime();
+        }
+
+        @Override
+        public long finishTime() {
+            return currentTime();
+        }
+
+        @Override
+        public long currentTime() {
+            long result = getCurrentTime();
+            result = incrementTimeIfstartTimeWasLastCall(result);
+            return result;
+        }
+
+        private long getCurrentTime() {
             Long result = currentTime.get();
-            result = result != null ? result : 0l;
-            currentTime.set(result + duration);
+            return result != null ? result : 0l;
+        }
+
+        private long incrementTimeIfstartTimeWasLastCall(long result) {
+            if (startTimeWasLastCall) {
+                result += duration;
+                currentTime.set(result);
+                startTimeWasLastCall = false;
+            }
             return result;
         }
     }
